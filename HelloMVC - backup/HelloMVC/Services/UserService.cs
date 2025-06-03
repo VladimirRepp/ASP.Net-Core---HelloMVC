b67d51e61e7c6272922ee9dd816264f3a8fd921c
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-06-03 18:48:08 +0700

    Add files via upload

---
+++ HelloMVC - backup/HelloMVC/Services/UserService.cs
@@ -0,0 +1,50 @@
+﻿using HelloMVC.Models;
+
+namespace HelloMVC.Services
+{
+    public class UserService
+    {
+        private List<User> _users;
+        private int _nextId = 1;
+
+        public UserService()
+        {
+            _users = new List<User>
+            {
+                new User { Id = GetCurrentId(), Login = "user1", Password = "pass1" },
+                new User { Id = GetCurrentId(), Login = "user2", Password = "pass2" }
+            };
+        }
+
+        private int GetCurrentId() => _nextId++;
+
+        public List<User> GetAll() => _users;
+
+        public User GetById(int id) => _users.FirstOrDefault(u => u.Id == id);
+
+        public void Add(User user)
+        {
+            user.Id = GetCurrentId();
+            _users.Add(user);
+        }
+
+        public void Update(User user)
+        {
+            var existingUser = GetById(user.Id);
+            if (existingUser != null)
+            {
+                existingUser.Login = user.Login;
+                existingUser.Password = user.Password;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var user = GetById(id);
+            if (user != null)
+            {
+                _users.Remove(user);
+            }
+        }
+    }
+}
